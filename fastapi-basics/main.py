29f4ca907801d224b2c39c9b8033cd0e7aaa4b81
Bhagyashree Pal <bhagyashreepal8485@gmail.com>
2026-09-24 23:30:37 +0530

    GET method started

---
+++ fastapi-basics/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, HTTPException
+
+app=FastAPI()
+
+items_list=["Book", "Pen", "Notebook", "Laptop"]
+
+@app.get("/")
+def root():
+    return {"message": "hello"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    if item_id<0 or item_id>=len(items_list):
+        raise HTTPException(status_code=404, detail=f"Item with ID {item_id} not found. Valid IDs are 0 to {len(items_list)-1}")
+
+    return {"item_id":item_id, "item_name":items_list[item_id]}
+
+
