224377d7ba3e72a8ff75ba68ba9eb869be7151ae
Bhagyashree Pal <bhagyashreepal8485@gmail.com>
2026-09-26 23:03:29 +0530

    importing module in main file

--- fastapi-basics/main.py
+++ fastapi-basics/main.py
@@ -1,21 +1,26 @@
 from fastapi import FastAPI, HTTPException
+import utils
 
 app=FastAPI()
 
-items_list=["Book", "Pen", "Notebook", "Laptop"]
+print(utils.greet("bhagyashree"))
 
-@app.get("/")
-def root():
-    return {"message": "hello"}
+print(utils.is_even(45))
+
+# items_list=["Book", "Pen", "Notebook", "Laptop"]
+
+# @app.get("/")
+# def root():
+#     return {"message": "hello"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    if item_id<0 or item_id>=len(items_list):
-        raise HTTPException(status_code=404, detail=f"Item with ID {item_id} not found. Valid IDs are 0 to {len(items_list)-1}")
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int):
+#     if item_id<0 or item_id>=len(items_list):
+#         raise HTTPException(status_code=404, detail=f"Item with ID {item_id} not found. Valid IDs are 0 to {len(items_list)-1}")
 
-    return {"item_id":item_id, "item_name":items_list[item_id]}
+#     return {"item_id":item_id, "item_name":items_list[item_id]}
 
-@app.get("/items")
-def read_item(skip:int=1, limit:int=2):
-    return items_list[skip : skip+limit]
+# @app.get("/items")
+# def read_item(skip:int=1, limit:int=2):
+#     return items_list[skip : skip+limit]
---
+++ fastapi-basics/utils.py
@@ -0,0 +1,8 @@
+def greet(name):
+    return f"Hello {name}"
+
+def is_even(num):
+    if num%2==0:
+        return f"{num} is even" 
+    else:
+        return f"{num} is odd"
